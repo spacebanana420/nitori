82f805837ce607f07bcdbdd4240254c71ce54a64
Space Banana <tomasbessa@protonmail.com>
2025-05-20 17:01:30 +0100

    Platform and root checks, test and more

--- src/nitori/main.java
+++ src/nitori/main.java
@@ -4,6 +4,11 @@ import nitori.cli.help;
 
 public class main {
   public static void main(String[] args) {
+    if (cli.askedForHelp(args)) {help.printHelp(); return;}
     help.printSmallHelp();
   }
+  
+  private static boolean supportedOS() {return System.getProperty("os.name").equals("Linux");}
+  
+  private static boolean isRoot() {return System.getProperty("user.home").equals("/root");}
 }
