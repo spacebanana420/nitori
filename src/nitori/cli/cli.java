4af6de9da83897c8e582f9fc7bddd6a0cbc167fe
Space Banana <tomasbessa@protonmail.com>
2025-05-20 16:01:26 +0100

    Initial work on CLI parsing and help screens

---
+++ src/nitori/cli/cli.java
@@ -0,0 +1,23 @@
+package nitori.cli;
+
+public class cli {
+}
+
+class parser {
+  static int findArgumentIndex(String[] args, String find_arg) {
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals(find_arg)) {return i;}
+    }
+    return -1;
+  }
+  
+  static boolean hasArgument(String[] args, String find_arg) {return findArgumentIndex(args, find_arg) != -1;}
+  
+  static String getArgumentValue(String[] args, String find_arg) {
+    int i = findArgumentIndex(args, find_arg);
+    if (i == -1 || i == args.length-1) {return null;}
+    String value = args[i+1].trim();
+    if (value.length() == 0) {return null;}
+    return value;
+  }
+}
---
+++ src/nitori/cli/help.java
@@ -0,0 +1,38 @@
+package nitori.cli;
+
+public class help {
+  private static final String VERSION = "0.1";
+  
+  public static void printHelp() {
+    System.out.println(
+      "Nitori version " + VERSION;
+      + "\nUsage: nitori <arguments>"
+      
+      + "\n\nList of arguments:"
+      + "\n  * -h (--help)                              Displays this screen"
+      
+      + "\n\n[CPU-related arguments]"
+      + "\n  * -cmin (--cpu-min) <clock speed>          Sets the minimum CPU clock speed in megahertz (MHz)"
+      + "\n  * -cmax (--cpu-min) <clock speed>          Sets the maximum CPU clock speed in megahertz (MHz)"
+      + "\n  * -cg (--cpu-governor) <governor>          Sets the CPU kernel governor"
+      + "\n  * -ci (--cpu-info)                         Displays info about the system's CPU and supported speeds and governors"
+      
+      + "\n\n[Battery-related arguments]"
+      + "\n  * -b (--battery-set) <percentage>          Sets the battery charge limit percentage if supported"
+      + "\n  * -bi (--battery-info)                     Displays info about the system's battery"
+      
+      + "\n\n[Backlight-related arguments]"
+      + "\n  * -l (--backlight-set) <percentage>        Sets the screen backlight brightness percentage"
+      + "\n  * -li (--backlight-info)                   Displays the current backlight percentage"
+    );
+  }
+  
+  public static void printSmallHelp() {
+    System.out.println(
+      "Nitori version " + VERSION
+      + "\nUsage: nitori <arguments>"
+      
+      + "\n\nRun \"nitori -h\" to see what you can do"
+    );
+  }
+}
