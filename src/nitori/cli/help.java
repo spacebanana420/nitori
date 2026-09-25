cf69e86b0df2ec1d880f7f716f9c4c11d52a002a
Space Banana <tomasbessa@protonmail.com>
2026-09-25 11:04:13 +0100

    Version update

--- src/nitori/cli/help.java
+++ src/nitori/cli/help.java
@@ -1,7 +1,7 @@
 package nitori.cli;
 
 public class help {
-  private static String title() {return "Nitori version 1.5";}
+  private static String title() {return "Nitori version 1.6";}
   
   public static void printHelp() {
     System.out.println(
