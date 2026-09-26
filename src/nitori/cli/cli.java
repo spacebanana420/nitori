91159f243f04a2e747e83c55ec4798474a5aab44
Space Banana <tomasbessa@protonmail.com>
2026-09-26 11:05:03 +0200

    Unique help screen for FreeBSD and change -cfreq to -cf

--- src/nitori/cli/cli.java
+++ src/nitori/cli/cli.java
@@ -15,7 +15,7 @@ public class cli {
     return new int[]{min, max};
   }
 
-  public static int cpuFrequency(String[] args) {return parser.getArgumentInt(args, "-cfreq", "--cpu-freq");}
+  public static int cpuFrequency(String[] args) {return parser.getArgumentInt(args, "-cf", "--cpu-freq");}
   
   public static String cpuGovernor(String[] args) {return parser.getArgumentValue(args, "-cg", "--cpu-governor");}
   public static String cpuEnergy(String[] args) {return parser.getArgumentValue(args, "-ce", "--cpu-energy-preference");}
--- src/nitori/cli/help.java
+++ src/nitori/cli/help.java
@@ -1,9 +1,16 @@
 package nitori.cli;
 
+import nitori.platform;
+
 public class help {
   private static String title() {return "Nitori version 1.6";}
-  
+
   public static void printHelp() {
+    if (platform.isFreeBSD()) printHelp_freebsd();
+    else printHelp_linux();
+  }
+  
+  public static void printHelp_linux() {
     System.out.println(
       title()
       + "\nUsage: nitori <arguments>"
@@ -12,9 +19,9 @@ public class help {
       + "\n  * -h (--help)                              Displays this screen"
       
       + "\n\n[CPU-related arguments]"
+      + "\n  * -cf (--cpu-freq) <clock speed>        Sets both the minimum and maximum clock speed in megahertz (MHz)"
       + "\n  * -cmin (--cpu-min) <clock speed>          Sets the minimum CPU clock speed in megahertz (MHz)"
       + "\n  * -cmax (--cpu-max) <clock speed>          Sets the maximum CPU clock speed in megahertz (MHz)"
-      + "\n  * -cfreq (--cpu-freq) <clock speed>        Sets both the minimum and maximum clock speed in megahertz (MHz)"
       + "\n  * -cg (--cpu-governor) <governor>          Sets the CPU kernel governor"
       + "\n  * -ce (--cpu-energy-preference) <mode>     Sets the CPU energy balance preference if available"
       + "\n  * -ci (--cpu-info)                         Displays info about the system's CPU and supported speeds and governors"
@@ -58,6 +65,77 @@ public class help {
       + "\n  * -q (--quiet)                             Disables printing to standard output"
       + "\n  * -v (--verbose)                           Displays more status messages on standard output"
       + "\n  * --debug                                  Displays even more status messages on standard output"
+      
+      + "\n\n[Symbol definitions]"
+      + "\n GB  Gigabytes"
+      + "\n Hz  Hertz"
+      + "\n MHz Megahertz"
+      + "\n W   Watt"
+      + "\n Wh  Watt-hour"
+      + "\n A   Ampere"
+      + "\n Ah  Ampere-hour"
+      + "\n V   Volt"
+    );
+  }
+
+  public static void printHelp_freebsd() {
+    System.out.println(
+      title()
+      + "\nUsage: nitori <arguments>"
+      
+      + "\n\nList of arguments:"
+      + "\n  * -h (--help)                              Displays this screen"
+      
+      + "\n\n[CPU-related arguments]"
+      + "\n  * -cf (--cpu-freq) <clock speed>        Sets both the minimum and maximum clock speed in megahertz (MHz)"
+      + "\n  * -ci (--cpu-info)                         Displays info about the system's CPU and supported speeds and governors"
+      + "\n  * -cr (--cpu-reset)                        Sets the minimum and maximum clock speeds"
+
+      + "\n\n[Memory-related arguments]"
+      + "\n  * -m (--memory-info)                       Displays OS memory size, usage and availability"
+      
+      + "\n\n[Battery-related arguments]"
+      + "\n  * -bi (--battery-info)                     Displays info about the system's battery"
+      
+      + "\n\n[Backlight-related arguments]"
+      + "\n  * -l (--backlight-set) <percentage>        Sets the screen backlight brightness percentage"
+      + "\n  * -li (--backlight-info)                   Displays the current backlight percentage"
+      + "\n  * -ls (--backlight-save)                   Saves the current backlight brightness so it can be restored later"
+      + "\n  * -lr (--backlight-restore)                Restores the last saved backlight brightness if available"
+      
+      + "\n\n[Suspension-related arguments]"
+      + "\n  * -s (--suspend)                           Suspends the system to RAM if available"
+      + "\n  * -s (--suspend) <state>                   Suspends the system according to a supported given state"
+      + "\n  * -ss (--suspend-states)                   Lists the supported and available suspend states"
+
+      + "\n\n[Preset-related arguments]"
+      + "\n  * -p (--preset) <preset name>              Executes a Nitori preset if available"
+      + "\n  * -pc (--create-preset) <preset name>      Creates a new preset file in /etc/nitori/"
+      + "\n  * -pl (--list-presets)                     Lists available presets if any exists"
+
+      + "\n\n[Process-related arguments]"
+      + "\n  * -proc (--process-count)                  Counts the amount of system processes and kernel threads"
+      + "\n  * -procl (--process-list)                  Lists the currently running system processes"
+      + "\n  * -procf (--process-find) <name>           Finds the processes that contain <name> in their path"
+
+      + "\n\n[Temperature-related arguments]"
+      + "\n  * -t (--temperature)                       Displays temperature values in degrees Celsius of known hardware sensors"
+      + "\n  * -ts (--temperature-simple)               Displays temperatures values just like -t/--temperature, but in a machine-readable format"
+      
+      + "\n\n[Other arguments]"
+      + "\n  * -q (--quiet)                             Disables printing to standard output"
+      + "\n  * -v (--verbose)                           Displays more status messages on standard output"
+      + "\n  * --debug                                  Displays even more status messages on standard output"
+
+      +"\n\n[Unsupported arguments]"
+      +"The functionality below is unsupported on FreeBSD"
+      + "\n  * -cmin (--cpu-min) <clock speed>          Sets the minimum CPU clock speed in megahertz (MHz)"
+      + "\n  * -cmax (--cpu-max) <clock speed>          Sets the maximum CPU clock speed in megahertz (MHz)"
+      + "\n  * -cg (--cpu-governor) <governor>          Sets the CPU kernel governor"
+      + "\n  * -ce (--cpu-energy-preference) <mode>     Sets the CPU energy balance preference if available"
+      + "\n  * -b (--battery-set) <percentage>          Sets the battery charge limit percentage if supported"
+      + "\n  * -gm (--gpu-memory)                       Displays VRAM and GTT usage of all connected GPUs"
+      + "\n  * -sh (--hibernate)                        Suspends the system by hibernating to disk"
       
       + "\n\n[Symbol definitions]"
       + "\n GB  Gigabytes"
